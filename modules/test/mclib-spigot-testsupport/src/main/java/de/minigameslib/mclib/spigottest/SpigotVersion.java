65f00247bfee4487f8052c4bd39214412319340e
mepeisen <mep_eisen@web.de>
2016-10-15 08:58:32 +0200

    beginning spigot server support

---
+++ modules/test/mclib-spigot-testsupport/src/main/java/de/minigameslib/mclib/spigottest/SpigotVersion.java
@@ -0,0 +1,63 @@
+/*
+    Copyright 2016 by minigameslib.de
+    All rights reserved.
+    If you do not own a hand-signed commercial license from minigames.de
+    you are not allowed to use this software in any way except using
+    GPL (see below).
+
+------
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+package de.minigameslib.mclib.spigottest;
+
+/**
+ * Spigot server version.
+ * 
+ * @author mepeisen
+ */
+public enum SpigotVersion
+{
+    
+    /** Latest supported version. */
+    Latest,
+    
+    /** V1.8 */
+    V1_8,
+    /** V1.8.3 */
+    V1_8_3,
+    /** V1.8.4 */
+    V1_8_4,
+    /** V1.8.5 */
+    V1_8_5,
+    /** V1.8.6 */
+    V1_8_6,
+    /** V1.8.7 */
+    V1_8_7,
+    /** V1.8.8 */
+    V1_8_8,
+    /** V1.9 */
+    V1_9,
+    /** V1.9.2 */
+    V1_9_2,
+    /** V1.9.4 */
+    V1_9_4,
+    /** V1.10 */
+    V1_10,
+    /** V1.10.2 */
+    V1_10_2
+    
+}
