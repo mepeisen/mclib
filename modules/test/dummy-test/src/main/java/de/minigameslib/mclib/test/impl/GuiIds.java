1f5bd13aee26eb04be1ee6a462f6a759725ab90f
mepeisen <mep_eisen@web.de>
2017-09-09 14:28:24 +0200

    infrastructure migration

--- modules/test/dummy-test/src/main/java/de/minigameslib/mclib/test/impl/GuiIds.java
+++
@@ -1,7 +0,0 @@
-package de.minigameslib.mclib.test.impl;
-
-import de.minigameslib.mclib.api.gui.AnvilGuiId;
-
-public enum GuiIds implements AnvilGuiId {
-	Sample
-}
--- modules/test/dummy-test/src/main/java/de/minigameslib/mclib/test/impl/GuiIds2.java
+++
@@ -1,7 +0,0 @@
-package de.minigameslib.mclib.test.impl;
-
-import de.minigameslib.mclib.api.gui.ClickGuiId;
-
-public enum GuiIds2 implements ClickGuiId {
-	Sample
-}
